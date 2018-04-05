d5d9ed1599fabe28f534d9a214b11091a3bcfa3f
Ryan Castle <ryan@hutsix.com.au>
2018-04-05 11:00:15 +0930

    Refactor test files

--- test/src/js/module1.js
+++
@@ -1,5 +0,0 @@
-import _ from 'lodash';
-
-export default function (arg = "default") {
-    console.log(_.join(["module1", arg], ' '));
-}
--- test/webpack.config.js
+++ test/webpack.config.js
@@ -1,61 +1,83 @@
 /* Plugins */
 const path = require('path');
+const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 /* Config */
-const OUTPUT_PATH = path.resolve(__dirname, 'public/dist');
-const OUTPUT_PUBLIC_PATH = process.env.ASSET_PATH || '/';
+const OUTPUT_PATH = path.resolve(__dirname, 'public/build');
+const OUTPUT_PUBLIC_PATH = (process.env.ASSET_PATH || '/') + 'build/';
 
-module.exports = {
-    entry: {'app': './src/index.js'},
-    module: {
-        rules: [
-            {
-                test: /\.s?css$/,
-                use: ExtractTextPlugin.extract(
-                    {
-                        use: ["css-loader", "sass-loader"],
-                        fallback: "style-loader"
-                    }
-                )
-            },
-            {
-                test: /\.tsx?$/,
-                use: {
-                    loader: 'ts-loader',
-                    options: {configFile: 'tsconfig.json'}
+module.exports = env => {
+    let isDevelopment = env && env.development;
+
+    return {
+        entry: {'app': './assets/js/app.ts'},
+        module: {
+            rules: [
+                {
+                    test: /\.s?css$/,
+                    use: ExtractTextPlugin.extract(
+                        {
+                            use: [
+                                {loader: "css-loader", options: {sourceMap: true}},
+                                {loader: "sass-loader", options: {sourceMap: true}}
+                            ],
+                            fallback: {
+                                loader: "style-loader",
+                                options: {sourceMap: true}
+                            }
+                        }
+                    )
                 },
-                exclude: /node_modules/,
-            },
-        ]
-    },
-    devServer: {
-        // contentBase: outputPath,
-        disableHostCheck: true,
-        host: "0.0.0.0",
-        watchOptions: {poll: 1000},
-    },
-    plugins: [
-        new ManifestPlugin({writeToFileEmit:true}),
-        new ExtractTextPlugin({filename: 'app.bundle.css'}),
-        new CleanWebpackPlugin([OUTPUT_PATH]),
-    ],
-    /* This is required to get webpack to recognise yarn global modules */
-    resolveLoader: {
-        modules: ["node_modules", process.env.NODE_PATH],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: OUTPUT_PATH,
-        publicPath: OUTPUT_PUBLIC_PATH,
-    },
-    watchOptions: {
-        poll: 1000,
-        aggregateTimeout: 100,
+                {
+                    test: /\.ts$/,
+                    use: {
+                        loader: 'ts-loader',
+                        options: {configFile: 'tsconfig.json'}
+                    },
+                }
+            ]
+        },
+        devtool: isDevelopment ? "cheap-module-eval-source-map" : "source-map",
+        devServer: {
+            disableHostCheck: true,
+            host: "0.0.0.0",
+            watchOptions: {poll: 500},
+            publicPath: OUTPUT_PUBLIC_PATH,
+            hot: true,
+            headers: {'Access-Control-Allow-Origin': '*'}
+        },
+        plugins: [
+            new ExtractTextPlugin({
+                filename: "[name].[hash].bundle.css",
+                disable: isDevelopment,
+            }),
+            new ManifestPlugin({writeToFileEmit: true}),
+            new CleanWebpackPlugin([OUTPUT_PATH]),
+            new webpack.NamedModulesPlugin(),
+            new webpack.HotModuleReplacementPlugin(),
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': JSON.stringify('production')
+            })
+        ],
+        /* This is required to get webpack to recognise yarn global modules */
+        resolveLoader: {
+            modules: ["node_modules", process.env.NODE_PATH],
+        },
+        resolve: {
+            modules: ["node_modules", process.env.NODE_PATH],
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        output: {
+            filename: '[name].[hash].bundle.js',
+            path: OUTPUT_PATH,
+            publicPath: OUTPUT_PUBLIC_PATH,
+        },
+        watchOptions: {
+            poll: 1000,
+            aggregateTimeout: 100,
+        }
     }
 };
